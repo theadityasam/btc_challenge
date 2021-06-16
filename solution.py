d9614842fcd8914652aa065d446a38a2fb4c3d0f
Aditya Samantaray <aditya.samantaray1@gmail.com>
2021-06-17 00:00:05 +0530

    Add valid mempool list creator

--- solution.py
+++ solution.py
@@ -1,5 +1,28 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('mempool.csv')
-print(df.head())
+
+class MempoolTransaction():
+    def __init__(self, txid, fee, weight, parents):
+        self.txid = txid
+        self.fee = int(fee)
+        # TODO: add code to parse weight and parents fields
+
+
+def parse_mempool_csv(filename):
+    """Parse the CSV file and return a list of MempoolTransactions."""
+    with open(filename) as f:
+        return([MempoolTransaction(*line.strip().split(',')) for line in f.readlines()])
+
+
+class Transaction():
+    def parse_mempool_csv(filename):
+        """Parse the CSV file and return a list of MempoolTransactions."""
+        with open(filename) as f:
+            parsed = [line.strip().split(',') for line in f.readlines()]
+            return ([MempoolTransaction(*x) for x in parsed if x[1].isnumeric()])
+
+
+# Transact =
+temp = parse_mempool_csv("mempool.csv")
+print(len(temp))
